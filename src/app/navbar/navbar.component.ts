bc8960d39a091a4152072c4e192a7ee7f5ec93c7
RoloufGoudebin <goudeloufrobin@gmail.com>
2021-07-16 19:22:14 +0200

    navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,9 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css','./navbar.component.scss']
+  styleUrls: ['./navbar.component.css', './navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  navLinks = [
+    { title: "Home", link: "/" },
+    { title: "Tarifs", link: "/tarifs" },
+    { title: "Nos activités", link: "/activites" },
+    { title: "Formations", link: "/formations" },
+    { title: "Liens", link: "/liens" },
+    { title: "Blog", link: "/blog" },
+    { title: "Contact", link: "/contact" }
+  ]
 
   constructor() { }
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,9 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $nav.toggleClass('scrolled shadow', $(this).scrollTop() > $nav.height());
     var $item = $(".nav-link");
     $item.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    console.lof("yooo");
+    var $item = $(".nav-link");
   });
 });
