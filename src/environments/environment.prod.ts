c09f36befc0f7d88c4a37ebee4cb49b2ee4eb891
Robin Goudelouf <goudeloufrobin@gmail.com>
2021-08-16 15:37:21 +0200

    ajout firebase env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDoFP-_bKIvczQsHC0vN2fwzIf4zKBnVHA",
+    authDomain: "belor-b14c1.firebaseapp.com",
+    databaseURL: "https://belor-b14c1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "belor-b14c1",
+    storageBucket: "belor-b14c1.appspot.com",
+    messagingSenderId: "1073554270886",
+    appId: "1:1073554270886:web:a92a2fc02537eea9c82267",
+    measurementId: "G-DLVJL5S7DJ"
+  }
 };
